3cb2c5381d8950cf9381da08d28b60e7ea03d589
biahv <beatriz.hviana@gmail.com>
2017-08-29 20:09:48 -0300

    Spring Security

--- src/main/java/com/algaworks/gestaofesta/controller/GestaoFestaController.java
+++ src/main/java/com/algaworks/gestaofesta/controller/GestaoFestaController.java
@@ -1,7 +1,9 @@
 package com.algaworks.gestaofesta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
@@ -9,6 +11,15 @@ public class GestaoFestaController {
 	@RequestMapping("")
 	public String home(){
 		return "home";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model, String error, String logout) {
+		if (error != null)
+			model.addAttribute("error", "Your username and password is invalid.");
+		if (logout != null)
+			model.addAttribute("message", "You have been logged out successfully.");
+		return "login";
 	}
 
 }
